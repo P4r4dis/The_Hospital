b50513da8a3b31f1033c4ec3cc95e1b5a34afc65
P4r4dis <adil.d.pro@gmail.com>
2022-12-23 17:22:45 +0100

    [ADD] - unit test for KoalaDoctorList::removeFromName added

--- part4_list/core_list/main_list.cpp
+++ part4_list/core_list/main_list.cpp
@@ -299,6 +299,17 @@ int     main(int ac, char **av)
 
         doc1.remove(&doc2);
         doc1.dump();
+
+        doc1.removeFromName("Boudur-Oulot");
+        koalaDoctor = doc1.getFromName("Boudur-Oulot");
+        
+        if (koalaDoctor == nullptr)
+		    std::cout << "GOOD = ERROR nullptr" << std::endl;
+        else if (koalaDoctor->getName() == "Boudur-Oulot")
+		    std::cout << "getFromName OK Boudur-Oulot found" << std::endl;
+        else
+		    std::cout << "ERROR " << koalaDoctor->getName() << " found instead of Boudur-Oulot" << std::endl;
+        doc1.dump();    
     }
     return 0;
 }
