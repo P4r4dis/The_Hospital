f6b52accfcd701aa21d820e15598bf8db596d7c3
P4r4dis <adil.d.pro@gmail.com>
2022-12-26 17:45:10 +0100

    [ADD] - unit test for addDoctor added.

--- part5_hospital/tests/tests_includes/test_my_hospital.hpp
+++ part5_hospital/tests/tests_includes/test_my_hospital.hpp
@@ -42,6 +42,36 @@ class Hospital
     {
         return _koalaDoctorList;
     }
+
+    void              addDoctor(KoalaDoctorList       *koalaDoctorList)
+    {
+        if (this->_koalaDoctorList && this->_koalaDoctorList->getFromName(koalaDoctorList->getDoctor()->getName())) {
+
+            this->_koalaDoctorList->append(koalaDoctorList);
+        }
+        else {
+            this->_koalaDoctorList = koalaDoctorList;
+        }
+        std::cout << "[HOSPITAL] Doctor " << _koalaDoctorList->getDoctor()->getName() << " just arrived!" << std::endl;
+        _koalaDoctorList->getDoctor()->timeCheck();
+    }
+
+
+    //         if (doc1.getDoctor() && doc1.getFromName(doc1.getDoctor()->getName()))
+    //     {
+    //             cr_log_warn("TEST");
+    //             bellevue.getKoalaDoctorList()->append(&doc1);
+    //             cr_assert(bellevue.getKoalaDoctorList() != nullptr);
+    //     }
+	// if (this->doctors) {
+	// 	this->doctors->append(doctor);
+	// }
+	// else {
+	// 	this->doctors = doctor;
+	// }
+	// this->action(doctor->getItem()->getName() + " just arrived !");
+	// doctor->getItem()->timeCheck();
+
 };
 
 #endif  // !__TEST_MY_HOSPITAL__
