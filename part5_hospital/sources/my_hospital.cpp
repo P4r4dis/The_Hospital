30037cca0dcea9d342afc6347e4a80c81021b836
P4r4dis <adil.d.pro@gmail.com>
2022-12-27 18:38:30 +0100

    [ADD] - dump() is added

--- part5_hospital/sources/my_hospital.cpp
+++ part5_hospital/sources/my_hospital.cpp
@@ -27,56 +27,46 @@ KoalaDoctorList     *Hospital::getKoalaDoctorList(void)
     return _koalaDoctorList;
 }
 
-    void            Hospital::addDoctor(KoalaDoctorList       *koalaDoctorList)
-    {
-        if (this->_koalaDoctorList && this->_koalaDoctorList->getFromName(koalaDoctorList->getDoctor()->getName())) {
-            return ;
-        }
+void                Hospital::addDoctor(KoalaDoctorList       *koalaDoctorList)
+{
+    if (this->_koalaDoctorList && this->_koalaDoctorList->getFromName(koalaDoctorList->getDoctor()->getName()))
+        return ;
+    if (this->_koalaDoctorList)
+            this->_koalaDoctorList->append(koalaDoctorList);
+    else
+        this->_koalaDoctorList = koalaDoctorList;
+    std::cout << "[HOSPITAL] Doctor " << koalaDoctorList->getDoctor()->getName() << " just arrived!" << std::endl;
+    koalaDoctorList->getDoctor()->timeCheck();
+}
 
-        if (this->_koalaDoctorList) {
-                this->_koalaDoctorList->append(koalaDoctorList);
-        }
-        else {
-            this->_koalaDoctorList = koalaDoctorList;
-        }
-        std::cout << "[HOSPITAL] Doctor " << koalaDoctorList->getDoctor()->getName() << " just arrived!" << std::endl;
-        koalaDoctorList->getDoctor()->timeCheck();
-    }
+void                Hospital::addSick(SickKoalaList       *sickKoalaList)
+{
+    if (this->_sickKoalaList && this->_sickKoalaList->getFromName(sickKoalaList->getPatient()->get_name()))
+        return ;
+    if (this->_sickKoalaList)
+        this->_sickKoalaList->append(sickKoalaList);
+    else
+        this->_sickKoalaList = sickKoalaList;
+    std::cout << "[HOSPITAL] Patient " << sickKoalaList->getPatient()->get_name() << " just arrived!" << std::endl;
+}
 
-        void        Hospital::addSick(SickKoalaList       *sickKoalaList)
-    {
-        if (this->_sickKoalaList && this->_sickKoalaList->getFromName(sickKoalaList->getPatient()->get_name())) {
-            return ;
-        }
-        if (this->_sickKoalaList) {
-            this->_sickKoalaList->append(sickKoalaList);
-        }
-        else {
-            this->_sickKoalaList = sickKoalaList;
-        }
-            std::cout << "[HOSPITAL] Patient " << sickKoalaList->getPatient()->get_name() << " just arrived!" << std::endl;
-    }
+void                Hospital::addNurse(KoalaNurseList       *koalaNurseList)
+{
+    if (this->_koalaNurseList && this->_koalaNurseList->getFromId(koalaNurseList->getNurse()->getID()))
+        return ;
+    if (this->_koalaNurseList)
+        this->_koalaNurseList->append(koalaNurseList);
+    else
+        this->_koalaNurseList = koalaNurseList;
+    std::cout << "[HOSPITAL] Nurse " << koalaNurseList->getNurse()->getID() << " just arrived!" << std::endl;
+    koalaNurseList->getNurse()->timeCheck();
+}
 
-    void            Hospital::addNurse(KoalaNurseList       *koalaNurseList)
-    {
-        if (this->_koalaNurseList && this->_koalaNurseList->getFromId(koalaNurseList->getNurse()->getID())) {
-            return ;
-        }
-        if (this->_koalaNurseList) {
-            this->_koalaNurseList->append(koalaNurseList);
-        }
-        else {
-            this->_koalaNurseList = koalaNurseList;
-        }
-        std::cout << "[HOSPITAL] Nurse " << koalaNurseList->getNurse()->getID() << " just arrived!" << std::endl;
-        koalaNurseList->getNurse()->timeCheck();
-    }
-
-    void            Hospital::dump()
-    {
-        std::cout << "[HOSPITAL] Work starting with:" << std::endl;
-        _koalaDoctorList->dump();
-        _koalaNurseList->dump();
-        _sickKoalaList->dump();
-        std::cout << std::endl;
-    }
+void                Hospital::dump()
+{
+    std::cout << "[HOSPITAL] Work starting with:" << std::endl;
+    _koalaDoctorList->dump();
+    _koalaNurseList->dump();
+    _sickKoalaList->dump();
+    std::cout << std::endl;
+}
